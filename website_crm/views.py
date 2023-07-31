077a1f56d4eb40a8bbb0716950355b74697bf3e0
abhishekghimire40 <abhishekghimire40@gmail.com>
2023-07-31 21:36:08 +0545

    added  a register html template and user registration functionality

--- website_crm/views.py
+++ website_crm/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
-
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -16,10 +16,9 @@ def home(request):
         if user is not None:
             login(request, user)
             messages.success(request, "You have been logged in succesfully!")
-            return redirect("home")
         else:
             messages.error(request, "There was an error logging in!")
-            return redirect("home")
+        return redirect("home")
     else:
         return render(request, "home.html", {})
 
@@ -28,3 +27,26 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been successfully logged out!")
     return redirect("home")
+
+
+def register_user(request):
+    # user submits the sign up form
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and log them in
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+
+            user = authenticate(request, username=username, password=password)
+            login(request,user)
+            messages.success(
+                request,
+                "Your account has been created successfully! You are logged in now!",
+            )
+            return redirect("home")
+    else:
+    # if user only visited this page
+        form = SignUpForm()
+    return render(request, "register.html", {"form": form})
